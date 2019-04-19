8251ed106e23b6492b7a36a81c8ca2acbd71a5ed
rachelhale21@gmail.com <rachelhale21@gmail.com>
2019-04-19 14:44:58 -0700

    add TwentyOneGame class

---
+++ TwentyOne/TwentyOne/TwentyOneGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class TwentyOneGame : Game // how you can inherit from another class 
+    {
+        public void Play()
+        {
+            throw new NotImplementedException();
+
+        }
+   
+    }
+}
