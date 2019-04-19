22760aa3b2b0a7f66414150606036b5cc23654ef
rachelhale21@gmail.com <rachelhale21@gmail.com>
2019-04-19 13:18:07 -0700

    add class Card

---
+++ TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+
+       public string Face {get; set;}
+       public string Suit {get; set;}
+    }
+}
