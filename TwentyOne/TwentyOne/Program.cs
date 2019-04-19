8d10e153fde3efc711c5964ab643700355a0be68
rachelhale21@gmail.com <rachelhale21@gmail.com>
2019-04-19 15:33:27 -0700

    add Player class and properties to it

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,22 +10,15 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Game game = new Game();
-            game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
-            game.ListPlayers();
-            
+            Deck deck = new Deck();
+            deck.Shuffle(3);
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
-            
-
-            //Deck deck = new Deck(); 
-            //deck.Shuffle(3);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            //Console.ReadLine();  
         }
     }
 }
