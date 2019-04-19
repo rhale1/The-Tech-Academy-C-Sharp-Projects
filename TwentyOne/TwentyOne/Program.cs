2b7131d01cf65a1ee7bd50ed7093d05bb47ccaa1
rachelhale21@gmail.com <rachelhale21@gmail.com>
2019-04-19 14:38:34 -0700

    add game class

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,25 +10,15 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Deck deck = new Deck(); // now it should have an entire deck of 52 cards
-            int timesShuffled = 0;
+            Deck deck = new Deck(); 
             deck.Shuffle(3);
-  
+
             foreach (Card card in deck.Cards)
-                {
+            {
                 Console.WriteLine(card.Face + " of " + card.Suit);
-                }
+            }
             Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();  
         }
-       
-        //public static Deck Shuffle(Deck deck, int times) // function for when someone wants to shuffle deck more than once
-        //{
-        //    for (int i = 0; i <times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
     }
 }
