1e655845ea71f510814d736b9d25789ac8505018
rachelhale21@gmail.com <rachelhale21@gmail.com>
2019-04-19 15:37:39 -0700

    Add Dealer class with properties and method

---
+++ TwentyOne/TwentyOne/Dealer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+  public class Dealer
+    {
+        public string Name { get; set; }
+        public Deck Deck { get; set; }
+        public int Balance { get; set; }
+
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards.First()); //grab the first hard
+            Console.WriteLine(Deck.Cards.First().ToString() + "\n"); // add to the hand that is passed into Deal
+
+        }
+    }
+}
