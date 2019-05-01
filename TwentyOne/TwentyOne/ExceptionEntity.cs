165ce1ae7bbd0118bacadbcfa84fa718c87239be
rachelhale21@gmail.com <rachelhale21@gmail.com>
2019-05-01 12:57:25 -0700

    add more ado.net

---
+++ TwentyOne/TwentyOne/ExceptionEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+   public class ExceptionEntity // entitiy commonly refereed to a class that maps exactly to a db
+    {
+        public int Id { get; set; }
+        public string ExceptinType { get; set; }
+        public string ExceptionMessage { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using Casino.TwentyOne;
 using System.Data.SqlClient;
 using System.Data;
+using System.Collections.Generic;
 
 namespace TwentyOne
  
@@ -16,7 +17,21 @@ namespace TwentyOne
 
             Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
+            if (playerName.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadExceptions(); // calls this method that reads all the exceptions from the db and assigns to list
+                foreach (var exception in Exceptions)
+                {
+                    Console.Write(exception.Id + " | ");
+                    Console.Write(exception.ExceptinType + " | ");
+                    Console.Write(exception.ExceptionMessage + " | ");
+                    Console.Write(exception.TimeStamp + " | ");
+                    Console.WriteLine();
+                }
 
+                Console.Read();
+                return;
+            }
             bool validAnswer = false;
             int bank = 0;
             while (!validAnswer)
@@ -95,6 +110,35 @@ namespace TwentyOne
                 command.ExecuteNonQuery(); // its a nonquery becuase insert statement
                 connection.Close();
             }
+        }
+        private static List<ExceptionEntity> ReadExceptions()
+        {
+            string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=TwentyOneGame;
+            Integrated Security=True;Connect Timeout=30;Encrypt=False;
+            TrustServerCertificate=False;ApplicationIntent=
+            ReadWrite;MultiSubnetFailover=False";
+
+            string queryString = @"Select Id, ExceptionType, ExceptionMessage, TimeStamp From Exceptions";
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+               SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while(reader.Read()) // loops through each record getting back
+                {
+                    ExceptionEntity exception = new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptinType = reader["ExceptionType"].ToString();
+                    exception.ExceptionMessage = reader["ExceptionMessage"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                    Exceptions.Add(exception); // was Exceptions.add(exception)
+
+                }
+                connection.Close();
+            }
+            return Exceptions;
         }
     }
 }
