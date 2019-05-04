be06edfc0c5afd303c4a3ab24465e6984b273568
rachelhale21@gmail.com <rachelhale21@gmail.com>
2019-05-03 18:28:50 -0700

    add vm

--- Newsletter/Newsletter/Controllers/HomeController.cs
+++ Newsletter/Newsletter/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newsletter.Models;
+using Newsletter.ViewModels;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -10,6 +13,10 @@ namespace Newsletter.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;
+                    Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                    TrustServerCertificate=False;ApplicationIntent=ReadWrite;
+                    MultiSubnetFailover=False";
         public ActionResult Index()
         {
             return View();
@@ -23,10 +30,7 @@ namespace Newsletter.Controllers
             }
             else
             {
-                string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;
-                    Integrated Security=True;Connect Timeout=30;Encrypt=False;
-                    TrustServerCertificate=False;ApplicationIntent=ReadWrite;
-                    MultiSubnetFailover=False";
+                
                 string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES 
                                         (@FirstName, @LastName, @EmailAddress)";
 
@@ -49,10 +53,40 @@ namespace Newsletter.Controllers
             }
         }
       
-
         public ActionResult Admin()
         {
-            return View();
+            string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber From Signups";
+            List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+               
+                 connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var signup = new NewsletterSignUp();
+                    signup.Id = Convert.ToInt32(reader["Id"]);
+                    signup.FirstName = reader["FirstName"].ToString();
+                    signup.LastName = reader["LastName"].ToString();
+                    signup.EmailAddress = reader["EmailAddress"].ToString();
+                    signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
+                    signups.Add(signup);
+                }
+            }
+            var signupVms = new List<SignupVm>();
+            foreach (var signup in signups)
+            {
+                var signupVm = new SignupVm();
+                signupVm.FirstName = signup.FirstName;
+                signupVm.LastName = signup.LastName;
+                signupVm.EmailAddress = signup.EmailAddress;
+                signupVms.Add(signupVm);
+            }
+                return View(signupVms);
         }
 
     }
--- Newsletter/Newsletter/ViewModels/SignupVm.cs
+++ Newsletter/Newsletter/ViewModels/SignupVm.cs
@@ -1,18 +1,16 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Newsletter.Models
+namespace Newsletter.ViewModels
 {
-    public class NewsletterSignUp
+    public class SignupVm
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
-        public string SocialSecurityNumber { get; set; }
+    
     }
-
 }
