bfd8bd7bfdbab48d601d0437f13f8484893251ad
rachelhale21@gmail.com <rachelhale21@gmail.com>
2019-05-05 11:39:57 -0700

    chaned method

--- Newsletter/Newsletter/Controllers/HomeController.cs
+++ Newsletter/Newsletter/Controllers/HomeController.cs
@@ -30,24 +30,15 @@ namespace Newsletter.Controllers
             }
             else
             {
-                
-                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES 
-                                        (@FirstName, @LastName, @EmailAddress)";
-
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (NewsletterEntities db = new NewsletterEntities())
                 {
-                    SqlCommand command = new SqlCommand(queryString, connection);
-                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
-                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
 
-                    command.Parameters["@FirstName"].Value = firstName;
-                    command.Parameters["@LastName"].Value = lastName;
-                    command.Parameters["@EmailAddress"].Value = emailAddress;
-
-                    connection.Open();
-                    command.ExecuteNonQuery();
-                    connection.Close();
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
                 }
                 return View("Success");
             }
@@ -55,39 +46,20 @@ namespace Newsletter.Controllers
       
         public ActionResult Admin()
         {
-            string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber From Signups";
-            List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (NewsletterEntities db = new NewsletterEntities())
             {
-                SqlCommand command = new SqlCommand(queryString, connection);
-               
-                 connection.Open();
-
-                SqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    var signup = new NewsletterSignUp();
-                    signup.Id = Convert.ToInt32(reader["Id"]);
-                    signup.FirstName = reader["FirstName"].ToString();
-                    signup.LastName = reader["LastName"].ToString();
-                    signup.EmailAddress = reader["EmailAddress"].ToString();
-                    signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
-                    signups.Add(signup);
+                var signups = db.SignUps;
+                var signupVms = new List<SignupVm>();
+                foreach (var signup in signups)
+                {                   
+                    var signupVm = new SignupVm();
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVms.Add(signupVm);
                 }
-            }
-            var signupVms = new List<SignupVm>();
-            foreach (var signup in signups)
-            {
-                var signupVm = new SignupVm();
-                signupVm.FirstName = signup.FirstName;
-                signupVm.LastName = signup.LastName;
-                signupVm.EmailAddress = signup.EmailAddress;
-                signupVms.Add(signupVm);
-            }
                 return View(signupVms);
+            }      
         }
-
     }
 }
